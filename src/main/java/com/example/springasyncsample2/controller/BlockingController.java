d5ed76111276b4f874b489b19a4dab75433daec5
glenn <glenn.jo@kakaocorp.com>
2020-09-15 09:37:28 +0900

    add Dockerfile

--- src/main/java/com/example/springasyncsample2/controller/BlockingController.java
+++ src/main/java/com/example/springasyncsample2/controller/BlockingController.java
@@ -3,12 +3,18 @@ package com.example.springasyncsample2.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class BlockingController {
 
-    @GetMapping("block")
-    public String blockingApi(String req) throws InterruptedException {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @GetMapping("some-api1")
+    public String someAPI1() throws InterruptedException {
+        String now = LocalDateTime.now().format(dtf);
         Thread.sleep(2000);
-        return req + "/blocking";
+        return String.format("some-api1 call, timestamp : %s", now);
     }
 }
